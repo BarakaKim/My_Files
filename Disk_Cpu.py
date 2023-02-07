eed130987ff52aa85f6986927ba2c26473e4cb46
barakakim <barrykim9891@gmail.com>
2023-02-07 22:10:29 +0300

    COMPUTER HEALTH CHECK

--- Disk_Cpu.py
+++ Disk_Cpu.py
@@ -9,7 +9,9 @@ print (f'FREE DISK:{(du.free/du.total)*100} %') #the percentage of free disk
                      #PSUTIL
 print (f'CPU PERCENTAGE: {psutil.cpu_percent(interval=1)}%')
 print (f'CPU COUNT:{psutil.cpu_count ()}')
+print ('\t')
 print (F'VIRTUAL MEMORY:\n {psutil.virtual_memory()}')
+print ('\t')
 print (f'dISK PARTIONS:\n {psutil.disk_partitions()}')
 print ('\t')
                     #INPUT FUNCTION
