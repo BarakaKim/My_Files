ba4671a483cce307c7ee11e222eb1dc069ae14aa
barakakim <barrykim9891@gmail.com>
2023-02-07 16:26:10 +0300

    NEW FILE COMMIT

--- Disk_Cpu.py
+++ Disk_Cpu.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
-                #BASICS OF SHUTIL AND PSUTIL
+                #SHUTIL AND PSUTIL MODULES
 import shutil
 import psutil
+                    #SHUTIL
 du = shutil.disk_usage ('/') # or shutil.disk_usage ('\\') using windows path
 #print (psutil.disk_usage('/')) #disk usage using psutil
 print (f'FREE DISK:{(du.free/du.total)*100} %') #the percentage of free disk
-
+                     #PSUTIL
 print (f'CPU PERCENTAGE: {psutil.cpu_percent(interval=1)}%')
 print (f'CPU COUNT:{psutil.cpu_count ()}')
 print (F'VIRTUAL MEMORY:\n {psutil.virtual_memory()}')
 print (f'dISK PARTIONS:\n {psutil.disk_partitions()}')
 print ('\t')
-
+                    #INPUT FUNCTION
+name = input ('To check your computer health, Please Enter your name:')
+password = int(input ('Please Enter your password:')) # Input function always return a string, to convert a string to integer, we use int
+print (f'Hello {name}')
+print ('\t')
                 #HEALTH CHECK IN A COMPUTER
-print ('HEALTH CHECK')
+print ('COMPUTER HEALTH STATUS')
 #functions for cheking cpu and disk health
 def check_disk_usage(disk):
     """Verifies that there's enough free space on disk"""
@@ -27,6 +32,6 @@ def check_cpu_usage():
     
 #If there's not enough disk, or not enough CPU, print an error
 if not check_disk_usage('/') or not check_cpu_usage():
-    print("ERROR!")
+    print("Your Computer is UNHEALTH: Check your disk or CPU usage  ")
 else:
-    print("Everything ok")
+    print('Your COMPUTER IS HEALTHY----Keep it up')
