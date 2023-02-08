15e6440513213e704f712d6ab1f4c915e2522f93
barakakim <barrykim9891@gmail.com>
2023-02-08 16:50:58 +0300

    Adding the testing_module.py file

--- test_basics.py
+++ test_basics.py
@@ -1,10 +1,19 @@
+name = input ('Enter your Name:')
+assert len (name) > 6, "your name has less than 6 characters"
+print ('Hello ' +  name + ',you are welcome')
+print ('\t')
+
 def test_sum():
     """ Testing the function of adding number"""
-    assert sum([1, 2, 3]) == 6, "The answer is 6"
+    assert sum([1, 2, 3]) == 6, "INCORRECT: The answer is 6" # syntax--- assert <expression>, <message if there is an error>
+    print ('TEST1: there is no error in summation function')
 
-def test_mult():
-    assert mult([1, 2, 3]) == 13, "incorect"
+assert pow(2,3) == 8, "INCORRECT: The answer is 8" # syntax--- assert <expression>, <message if there is an error>
+print ('TEST2: there is no error in a function for exponents')
 
+assert (2*2) == 4, "INCORRECT: The answer should be 4"
+print ('TEST3: there is no error in Square function')
+print ('\t')
 if __name__ == "__main__":
     test_sum()
     print("Everything passed")
