ce1d5a31871c530233b7053ad1a50e8aabe8039c
barakakim <barrykim9891@gmail.com>
2023-02-08 12:02:48 +0300

    NEW FILE ADDED

--- Disk_Cpu.py
+++ Disk_Cpu.py
@@ -17,6 +17,7 @@ print ('\t')
                     #INPUT FUNCTION
 name = input ('To check your computer health, Please Enter your name:')
 password = int(input ('Please Enter your password:')) # Input function always return a string, to convert a string to integer, we use int
+assert password == 12345, 'The password is incorrect'
 print (f'Hello {name}')
 print ('\t')
                 #HEALTH CHECK IN A COMPUTER
@@ -31,7 +32,7 @@ def check_cpu_usage():
     """Verifies that there's enough unused CPU"""
     usage = psutil.cpu_percent(1)
     return usage < 75
-    
+
 #If there's not enough disk, or not enough CPU, print an error
 if not check_disk_usage('/') or not check_cpu_usage():
     print("Your Computer is UNHEALTH: Check your disk or CPU usage  ")
